07de87a469ee6d6c3455628378514f72c6877f68
Anri Liy <amedamachip713@gmail.com>
2020-07-16 15:06:47 -0700

    JWT Generator

--- server/routes/jwtAuth.js
+++ server/routes/jwtAuth.js
@@ -1,11 +1,13 @@
 const router = require("express").Router();
 const pool = require("../db");
-const bcrypt = require("bcrypt")
+const bcrypt = require("bcrypt");
+const jwtGenerator = require("../utils/jwtGenerator");
 
 router.post("/register", async (req, res) => {
   try {
 
     const { name, email, password } = req.body;
+
     const user = await pool.query("SELECT * FROM users WHERE email = $1", [email]);
     
     if(user.rows.length !== 0) {
@@ -17,8 +19,13 @@ router.post("/register", async (req, res) => {
 
     const bcryptPassword = await bcrypt.hash(password, salt);
 
-    const neweUser = await pool.query("INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING *", [name, email, bcryptPassword])
-    res.json(neweUser.rows[0]);
+    const newUser = await pool.query("INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING *", [name, email, bcryptPassword])
+
+    // res.json(newUser.rows[0].id);
+
+    const token = jwtGenerator(newUser.rows[0].id);
+
+    res.json({token});
 
   } catch(err) {
     console.error(err.message);
