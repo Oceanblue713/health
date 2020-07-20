4702fd46ab47ebd8ce41682d9523ad92521dfed4
Anri Liy <amedamachip713@gmail.com>
2020-07-19 17:33:52 -0700

    Login Route

--- server/routes/jwtAuth.js
+++ server/routes/jwtAuth.js
@@ -33,4 +33,29 @@ router.post("/register", async (req, res) => {
   }
 });
 
+router.post("/login", async (req, res) => {
+  try {
+
+    const {email, password} = req.body;
+
+    const user = await pool.query("SELECT * FROM users WHERE email = $1", [email]);
+
+    if(user.rows.length === 0){
+      return res.status(401).json("Password or Emails is incorrect");
+    }
+
+    const validPassword = await bcrypt.compare(password, user.rows[0].password);
+
+    if (!validPassword) {
+      return res.status(401).json("Password or Email is incorrect");
+    }
+
+    const token = jwtGenerator(user.rows[0].id);
+    res.json({token});
+
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
 module.exports = router;
