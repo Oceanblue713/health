d6a002cbdb0971f489f924e8aa87ff9a00d2288b
Anri Liy <amedamachip713@gmail.com>
2020-07-31 13:08:43 -0700

    Login Component

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,4 +1,5 @@
 import React, { Fragment, useState } from 'react';
+import {Link} from 'react-router-dom'
 
 const Register = ({ setAuth }) => {
 
@@ -44,6 +45,7 @@ const Register = ({ setAuth }) => {
         <input type="text" name="name" placeholder="name" className="form-control my-3" value={name} onChange={(e) => onChange(e)}/>
         <button className="btn btn-success btn-block">Submit</button>
       </form>
+      <Link to="/login">Login</Link>
     </Fragment>
   )
 }
